9b3218e78ece96547924396593a9a07319ff4c6d
HaruGD <unity8392@gmail.com>
2026-05-24 16:06:12 +0900

    Loop user-mode input test until q

--- src/boot/user64.asm
+++ src/boot/user64.asm
@@ -31,6 +31,35 @@ user_test_code_start:
     lea rdi, [rel user_msg]
     mov rsi, user_msg_end - user_msg
     int 0x80
+    mov rax, 1
+    lea rdi, [rel user_prompt]
+    mov rsi, user_prompt_end - user_prompt
+    int 0x80
+    mov rax, 1
+    lea rdi, [rel user_hint]
+    mov rsi, user_hint_end - user_hint
+    int 0x80
+.wait_key:
+    mov rax, 4
+    int 0x80
+    mov [rel user_last_char], al
+    mov rax, 1
+    lea rdi, [rel user_pressed_msg]
+    mov rsi, user_pressed_msg_end - user_pressed_msg
+    int 0x80
+    movzx rdi, byte [rel user_last_char]
+    mov rax, 3
+    int 0x80
+    mov rax, 1
+    lea rdi, [rel user_newline]
+    mov rsi, user_newline_end - user_newline
+    int 0x80
+    cmp byte [rel user_last_char], 'q'
+    jne .wait_key
+    mov rax, 1
+    lea rdi, [rel user_exit_msg]
+    mov rsi, user_exit_msg_end - user_exit_msg
+    int 0x80
     mov rax, 2
     int 0x80
 .halt:
@@ -38,6 +67,23 @@ user_test_code_start:
 user_msg:
     db 'Hello from user mode via syscall', 10
 user_msg_end:
+user_prompt:
+    db 'User-mode input loop ready.', 10
+user_prompt_end:
+user_hint:
+    db 'Press keys in user mode. Press q to exit.', 10
+user_hint_end:
+user_pressed_msg:
+    db 'You pressed: '
+user_pressed_msg_end:
+user_newline:
+    db 10
+user_newline_end:
+user_exit_msg:
+    db 'Leaving user mode...', 10
+user_exit_msg_end:
+user_last_char:
+    db 0
 user_test_code_end:
 
 enter_user_mode:
